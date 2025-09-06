962d588bb7a093214067048f5ec74c75c9a8b584
AslamThachapalli <aslam.develop912@gmail.com>
2025-09-06 21:03:25 +0530

    Added ignition module for contrac deployment using hardhat. Add frontend setup with React, TypeScript, Vite, tailwind and shadcn.

---
+++ hardhat/ignition/modules/MultiSigWallet.ts
@@ -0,0 +1,15 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+export default buildModule("MultiSigWallet", (m) => {
+    const owner1 = m.getAccount(0);
+    const owner2 = m.getAccount(1);
+    const owner3 = m.getAccount(2);
+    const owner4 = m.getAccount(3);
+    
+    const msWallet = m.contract("MultiSigWallet", [
+        [owner1, owner2, owner3, owner4],
+        3,
+    ]);
+
+    return { msWallet };
+});
